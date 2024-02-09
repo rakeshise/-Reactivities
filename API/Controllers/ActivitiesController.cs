6c1c601bf385efa35174911f260a1fd06c21390c
Rakesh <rakeshise@gmail.com>
2024-02-09 18:15:45 +0530

    End of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController:BaseApiController
+    {
+        private readonly DataContext _context;
+       
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] //api//activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api//activities
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+#pragma warning disable CS8604 // Possible null reference argument.
+            return await _context.Activities.FindAsync(id);
+#pragma warning restore CS8604 // Possible null reference argument.
+        }
+    }
+}
